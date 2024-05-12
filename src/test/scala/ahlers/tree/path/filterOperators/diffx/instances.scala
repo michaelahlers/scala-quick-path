a6a0c8f3a1e51c95eb36f359e795703d64dcf750
Michael Ahlers <michael@ahlers.consulting>
2024-05-12 17:50:14 -0600

    Finish all test support

--- src/test/scala/ahlers/tree/path/filterOperators/diffx/instances.scala
+++ src/test/scala/ahlers/tree/path/filterOperators/diffx/instances.scala
@@ -1,15 +1,25 @@
 package ahlers.tree.path.filterOperators.diffx
 
-import ahlers.tree.path.filterOperators.{FilterOperator, IsEqualTo, IsNotEqualTo}
+import ahlers.tree.path.filterOperators._
 import ahlers.tree.path.filterOperators.algebra.IsFilterOperator
-import ahlers.tree.path.terms.diffx.instances._
 import com.softwaremill.diffx.Diff
 
 object instances {
 
-  implicit val diffIsEqual: Diff[IsEqualTo.type] = Diff.derived
-
-  implicit val diffIsNotEqual: Diff[IsNotEqualTo.type] = Diff.derived
+  implicit val diffIsEqual: Diff[IsEqualTo.type]                         = Diff.derived
+  implicit val diffIsNotEqual: Diff[IsNotEqualTo.type]                   = Diff.derived
+  implicit val diffIsLessThan: Diff[IsLessThan.type]                     = Diff.derived
+  implicit val diffIsLessThanOrEqual: Diff[IsLessThanOrEqual.type]       = Diff.derived
+  implicit val diffIsGreaterThan: Diff[IsGreaterThan.type]               = Diff.derived
+  implicit val diffIsGreaterThanOrEqual: Diff[IsGreaterThanOrEqual.type] = Diff.derived
+  implicit val diffIsMatchOf: Diff[IsMatchOf.type]                       = Diff.derived
+  implicit val diffIsIn: Diff[IsIn.type]                                 = Diff.derived
+  implicit val diffIsNotIn: Diff[IsNotIn.type]                           = Diff.derived
+  implicit val diffIsSubsetOf: Diff[IsSubsetOf.type]                     = Diff.derived
+  implicit val diffIsAnyOf: Diff[IsAnyOf.type]                           = Diff.derived
+  implicit val diffIsNoneOf: Diff[IsNoneOf.type]                         = Diff.derived
+  implicit val diffHasSize: Diff[HasSize.type]                           = Diff.derived
+  implicit val diffIsEmpty: Diff[IsEmpty.type]                           = Diff.derived
 
   implicit val diffFilterOperator: Diff[FilterOperator] = {
     implicit val diffIsFilterOperator: Diff[IsFilterOperator] = {
--- src/test/scala/ahlers/tree/path/filterOperators/scalacheck/instances.scala
+++ src/test/scala/ahlers/tree/path/filterOperators/scalacheck/instances.scala
@@ -1,23 +1,42 @@
 package ahlers.tree.path.filterOperators.scalacheck
 
-import ahlers.tree.path.filterOperators.FilterOperator
-import ahlers.tree.path.filterOperators.IsEqualTo
-import ahlers.tree.path.filterOperators.algebra.IsFilterOperator
-import ahlers.tree.path.terms.Index
-import ahlers.tree.path.terms.Name
-import ahlers.tree.path.terms.scalacheck.instances._
-import magnolify.scalacheck.semiauto._
+import ahlers.tree.path.filterOperators._
 import org.scalacheck.Arbitrary
 import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.Gen
 
 object instances {
 
-  implicit val arbEqual: Arbitrary[IsEqualTo.type] = Arbitrary(Gen.const(IsEqualTo))
+  implicit val arbIsEqual: Arbitrary[IsEqualTo.type]                         = Arbitrary(Gen.const(IsEqualTo))
+  implicit val arbIsNotEqual: Arbitrary[IsNotEqualTo.type]                   = Arbitrary(Gen.const(IsNotEqualTo))
+  implicit val arbIsLessThan: Arbitrary[IsLessThan.type]                     = Arbitrary(Gen.const(IsLessThan))
+  implicit val arbIsLessThanOrEqual: Arbitrary[IsLessThanOrEqual.type]       = Arbitrary(Gen.const(IsLessThanOrEqual))
+  implicit val arbIsGreaterThan: Arbitrary[IsGreaterThan.type]               = Arbitrary(Gen.const(IsGreaterThan))
+  implicit val arbIsGreaterThanOrEqual: Arbitrary[IsGreaterThanOrEqual.type] = Arbitrary(Gen.const(IsGreaterThanOrEqual))
+  implicit val arbIsMatchOf: Arbitrary[IsMatchOf.type]                       = Arbitrary(Gen.const(IsMatchOf))
+  implicit val arbIsIn: Arbitrary[IsIn.type]                                 = Arbitrary(Gen.const(IsIn))
+  implicit val arbIsNotIn: Arbitrary[IsNotIn.type]                           = Arbitrary(Gen.const(IsNotIn))
+  implicit val arbIsSubsetOf: Arbitrary[IsSubsetOf.type]                     = Arbitrary(Gen.const(IsSubsetOf))
+  implicit val arbIsAnyOf: Arbitrary[IsAnyOf.type]                           = Arbitrary(Gen.const(IsAnyOf))
+  implicit val arbIsNoneOf: Arbitrary[IsNoneOf.type]                         = Arbitrary(Gen.const(IsNoneOf))
+  implicit val arbHasSize: Arbitrary[HasSize.type]                           = Arbitrary(Gen.const(HasSize))
+  implicit val arbIsEmpty: Arbitrary[IsEmpty.type]                           = Arbitrary(Gen.const(IsEmpty))
 
   implicit val arbFilterOperator: Arbitrary[FilterOperator] = Arbitrary(Gen.oneOf(
     arbitrary[IsEqualTo.type],
-    arbitrary[IsEqualTo.type],
+    arbitrary[IsNotEqualTo.type],
+    arbitrary[IsLessThan.type],
+    arbitrary[IsLessThanOrEqual.type],
+    arbitrary[IsGreaterThan.type],
+    arbitrary[IsGreaterThanOrEqual.type],
+    arbitrary[IsMatchOf.type],
+    arbitrary[IsIn.type],
+    arbitrary[IsNotIn.type],
+    arbitrary[IsSubsetOf.type],
+    arbitrary[IsAnyOf.type],
+    arbitrary[IsNoneOf.type],
+    arbitrary[HasSize.type],
+    arbitrary[IsEmpty.type],
   ))
 
 }

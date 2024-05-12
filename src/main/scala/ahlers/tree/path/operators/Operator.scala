a9a1dc3579924baa415d030d3345f938ba5bfc5c
Michael Ahlers <michael@ahlers.consulting>
2024-05-12 17:38:02 -0600

    Add references

--- src/main/scala/ahlers/tree/path/operators/Operator.scala
+++ src/main/scala/ahlers/tree/path/operators/Operator.scala
@@ -1,5 +1,8 @@
 package ahlers.tree.path.operators
 
+/**
+ * @see [[https://github.com/json-path/JsonPath/blob/45333e0a310af70ad48d34d306da30af1e8e6314/README.md#operators]]
+ */
 trait Operator {
   def toText: String
 }
